313d59c3f5d124fc5d3366b7fb1722f0192ce015
Fabio Maciel <fabio.souza@metasix.com.br>
2016-07-27 17:08:32 -0300

    Added Summoner basic methods

--- src/summoner.js
+++ src/summoner.js
@@ -5,15 +5,31 @@ var DefaultCli = require('./default')
 const API_VERSION = 'v1.4'
 const ROOT = 'summoner'
 
+function byParam(param, self){
+	var options = self.getOptions(param);
+	return self.get(options)
+}
+
 class Summoner extends DefaultCli{
 
 	constructor(key){
 		super(key, ROOT, API_VERSION)
 	}
 
-	byName (name){  
-		var options = this.getOptions(`by-name/${name}`);
-		return this.get(options)
+	byName (name){
+		return byParam(`by-name/${name}`, this)
+	}
+
+	byId (id){  
+		return byParam(id, this)
+	}
+
+	nameById (id){  
+		return byParam(`${id}/name`, this)
+	}
+
+	runesById (id){  
+		return byParam(`${id}/runes`, this)
 	}
 }
 
