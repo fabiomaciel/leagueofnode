50451b49a860c7420b2321b5a373f38685fc86e8
Fabio Maciel <fabio.souza@metasix.com.br>
2016-07-28 18:10:11 -0300

    Added multiple language

--- index.js
+++ index.js
@@ -8,11 +8,16 @@ const Summoner = require('./src/summoner'),
 
 class LeagueOfNode{
     constructor(key){
-        this.summoner = new Summoner(key)
-        this.matchlist = new MatchList(key)
-        this.match = new Match(key)
-        this.champion = new Champion(key)
-        this.staticData = new StaticData(key)
+        ['BR', 'EUNE', 'EUW', 'JP', 'KR', 'LAN', 'LAS', 'NA', 'OCE', 'RU', 'TR']
+            .forEach(e => {
+                e = e.toLowerCase()
+                this[e] = {}
+                this[e].summoner = new Summoner(key, e)
+                this[e].matchlist = new MatchList(key, e)
+                this[e].match = new Match(key, e)
+                this[e].champion = new Champion(key, e)
+                this[e].staticData = new StaticData(key, e)
+        })
     }
 }
 
